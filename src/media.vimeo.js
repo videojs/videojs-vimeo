e6553675fbd156ae5518662965105cbd635c4630
Prabhu Subramanian <prabhu@colearnr.com>
2014-10-11 20:57:07 +0100

    Added currentSrc method

--- src/media.vimeo.js
+++ src/media.vimeo.js
@@ -9,7 +9,7 @@ var VimeoState = {
   PAUSED: 2,
   BUFFERING: 3
 };
- 
+
 /**
  * Vimeo Media Controller - Wrapper for Vimeo Media API
  * @param {videojs.Player|Object} player
@@ -20,13 +20,13 @@ var VimeoState = {
 videojs.Vimeo = videojs.MediaTechController.extend({
   init: function(player, options, ready){
     videojs.MediaTechController.call(this, player, options, ready);
-    
+
     this.player_ = player;
     this.player_el_ = document.getElementById(this.player_.id());
 
     // Disable lockShowing because we always use Vimeo controls
     this.player_.controls(false);
-    
+
     this.id_ = this.player_.id() + '_vimeo_api';
 
     this.el_ = videojs.Component.prototype.createEl('iframe', {
@@ -40,9 +40,9 @@ videojs.Vimeo = videojs.MediaTechController.extend({
       mozallowfullscreen: 'true',
       allowFullScreen: 'true'
     });
-    
+
     this.player_el_.insertBefore(this.el_, this.player_el_.firstChild);
-    
+
     this.baseUrl = (document.location.protocol == 'https')? 'https://secure.vimeo.com/video/' : 'http://player.vimeo.com/video/';
 
     this.vimeo = {};
@@ -61,13 +61,13 @@ videojs.Vimeo.prototype.dispose = function(){
   this.vimeo.api('unload');
   delete this.vimeo;
   this.el_.parentNode.removeChild(this.el_);
-  
+
   videojs.MediaTechController.prototype.dispose.call(this);
 };
 
 videojs.Vimeo.prototype.src = function(src){
   this.isReady_ = false;
-  
+
   // Regex that parse the video ID for any Vimeo URL
   var regExp = /^.*(vimeo\.com\/)((channels\/[A-z]+\/)|(groups\/[A-z]+\/videos\/))?([0-9]+)/;
   var match = src.match(regExp);
@@ -75,7 +75,7 @@ videojs.Vimeo.prototype.src = function(src){
   if (match){
     this.videoId = match[5];
   }
-  
+
   var params = {
     api: 1,
     byline: 0,
@@ -88,7 +88,7 @@ videojs.Vimeo.prototype.src = function(src){
     autoplay: (this.player_.options()['autoplay'])?1:0,
     loop: (this.player_.options()['loop'])?1:0
   };
-  
+
   this.el_.src = this.baseUrl + this.videoId + '?' + videojs.Vimeo.makeQueryString(params);
 };
 
@@ -116,15 +116,17 @@ videojs.Vimeo.prototype.setVolume = function(percentAsDecimal){
   this.vimeoInfo.volume = percentAsDecimal;
   this.player_.trigger('volumechange');
 };
-
+videojs.Vimeo.prototype.currentSrc = function() {
+  return this.el_.src;
+};
 videojs.Vimeo.prototype.muted = function() { return this.vimeoInfo.muted || false; };
-videojs.Vimeo.prototype.setMuted = function(muted) { 
+videojs.Vimeo.prototype.setMuted = function(muted) {
   if (muted) {
     this.vimeoInfo.muteVolume = this.vimeoInfo.volume;
     this.setVolume(0);
-  } else { 
+  } else {
     this.setVolume(this.vimeoInfo.muteVolume);
-  } 
+  }
 
   this.vimeoInfo.muted = muted;
   this.player_.trigger('volumechange');
@@ -133,7 +135,7 @@ videojs.Vimeo.prototype.setMuted = function(muted) {
 videojs.Vimeo.prototype.onReady = function(){
   this.isReady_ = true;
   this.triggerReady();
-  
+
   if (this.startMuted) {
     this.setMuted(true);
     this.startMuted = false;
@@ -145,9 +147,9 @@ videojs.Vimeo.prototype.onLoad = function(){
     this.vimeo.api('unload');
     delete this.vimeo;
   }
-  
+
   this.vimeo = $f(this.el_);
-  
+
   this.vimeoInfo = {
     state: VimeoState.UNSTARTED,
     volume: 1,
@@ -159,7 +161,7 @@ videojs.Vimeo.prototype.onLoad = function(){
     url: this.baseUrl + this.videoId,
     error: null
   };
-  
+
   var self = this;
   this.vimeo.addEvent('ready', function(id){ self.onReady(); });
   this.vimeo.addEvent('loadProgress', function(data, id){ self.onLoadProgress(data); });
