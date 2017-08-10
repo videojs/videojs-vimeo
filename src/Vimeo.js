c896cc06923b1cb7a186b38ae777d4a89b1d5c23
Sergey Fursov <geyser85@gmail.com>
2017-08-10 12:07:29 +0300

    properly initialize Vimeo tech, compatible with video.js 6.x

--- src/Vimeo.js
+++ src/Vimeo.js
@@ -2,7 +2,7 @@ import videojs from 'video.js';
 import VimeoPlayer from '@vimeo/player';
 
 const Component = videojs.getComponent('Component');
-const Tech = videojs.getComponent('Tech');
+const Tech = videojs.getTech('Tech');
 let cssInjected = false;
 
 // Since the iframe can't be touched using Vimeo's way of embedding,
@@ -263,8 +263,11 @@ Vimeo.nativeSourceHandler.dispose = function() { };
 
 Vimeo.registerSourceHandler(Vimeo.nativeSourceHandler);
 
-Component.registerComponent('Vimeo', Vimeo);
-Tech.registerTech('Vimeo', Vimeo);
+if (typeof Tech.registerTech !== 'undefined') {
+  Tech.registerTech('Vimeo', Vimeo);
+} else {
+  Component.registerComponent('Vimeo', Vimeo);
+}
 
 // Include the version number.
 Vimeo.VERSION = '0.0.1';
